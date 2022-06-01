9d3ef20784fcdc239cb535ef1108f9bcffe5d4f2
MarioJoao31 <mario.joao31@hotmail.com>
2022-06-01 18:09:21 +0100

    bot almost full run

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def minBelt():
     pyautogui.moveTo(buttonAsteroidBeltX, buttonAsteroidBeltY)
     pyautogui.click()
     print("Mining belt locked...")
-    time.sleep(20)
+    time.sleep(35)
 
 def warp():
     #go to mining belt
@@ -41,11 +41,29 @@ def warp():
     print("Warping...")
     time.sleep(2)
 
+def warp2():
+    #go to mining belt
+    pyautogui.press('s') 
+    print("Warping...")
+    time.sleep(35)
+
 #see if the ore hold is full 
 def fullOreHold():
-    im = pyautogui.screenshot()
-    pyautogui.moveTo(339, 590)
-    im.getpixel((339,590))
+    if(pyautogui.pixelMatchesColor(339, 590, (3, 71, 91))):
+        print("Ore Hold Full...")
+        generalOverview()
+        mininStationOverview()
+        warp2()
+        dock()
+        transferMiningHold()
+    else:
+        print("Ore Hold Not full...")
+        scan()
+        scanedVeldpar5k()
+        aproch()
+        scanedVeldpar5k()
+        lock()
+        ActivateMiners()
 
 def generalOverview():
     #general Overview
@@ -72,7 +90,7 @@ def dock():
     time.sleep(1)
 
 #transfers items from mining ore to item hangar 
-def miningHold():
+def transferMiningHold():
 
     pyautogui.moveTo(145, 411)
     pyautogui.click()
@@ -80,8 +98,8 @@ def miningHold():
     pyautogui.moveTo(213, 495)
     pyautogui.dragTo(547, 390, 2, button='left')
     pyautogui.moveTo(234, 426)
-    pyautogui.dragTo(142, 452, 2, button='left')
-    print("items transfered..")
+    pyautogui.dragTo(135, 475, 2, button='left')
+    print("items transfered...")
 
 def lockVeldspar():
     #localiza o centro da imagem UNDOCK
@@ -93,6 +111,57 @@ def lockVeldspar():
     print("Locked veldspar...")
     time.sleep(1)
 
+def scanedVeldpar5k():
+    #localiza o centro da imagem UNDOCK
+    buttonLockVeldPos = pyautogui.locateOnScreen('scanedVeldspar.png', confidence=0.8)
+    buttonLockVeld=pyautogui.center(buttonLockVeldPos)
+    btLx, btLy = buttonLockVeld
+    pyautogui.moveTo(btLx, btLy)
+    pyautogui.click()
+    print("Locked veldspar...")
+    time.sleep(2)
+    aproch()
+    lock()
+
+def scan():
+    #localiza o centro da imagem UNDOCK
+    buttonScanPos = pyautogui.locateOnScreen('scan.png', confidence=0.8)
+    buttonScan=pyautogui.center(buttonScanPos)
+    btSx, btSy = buttonScan
+    pyautogui.moveTo(btSx, btSy)
+    pyautogui.click()
+    print("Scanning asteroid belt 7sec")
+    time.sleep(8)
+
+def aproch():
+    #go to mining belt
+    btAx=1585
+    btAy=109
+    
+    pyautogui.moveTo(btAx, btAy)
+    pyautogui.click()
+    print("Aproching...")
+    time.sleep(30)
+
+def lock():
+    #go to mining belt
+    btLcx=1707
+    btLcy=112
+    pyautogui.moveTo(btLcx, btLcy)
+    pyautogui.click()
+    print("Target Locked...")
+    time.sleep(2)
+
+def ActivateMiners():
+    #go to mining belt
+    pyautogui.hotkey('f1') 
+    print("Miner 1 Locked...")
+    pyautogui.hotkey('f2') 
+    print("Miner 2 Locked...")
+    for each in range(0,360):
+        print("Mining for ", each ," sec")
+        time.sleep(1)
+    fullOreHold()
 
 
 
@@ -108,21 +177,18 @@ def main():
     #######
 
    
-    ######funçoes######
-    #undock()
-    #minOverview()
-    #warp()
-    #minBelt()
-    lockVeldspar()
-    #fullOreHold()
-
-
-
-    #generalOverview()
-    #mininStationOverview()
-    #warp()
-    #dock()
-    #miningHold()
+    #for i in range(0,5):
+        #print("------Reset nº",i," Mining again------")
+        #undock()
+        #minOverview()
+        #warp()
+        #minBelt()
+        #lockVeldspar()
+        #aproch()
+        #scan()      
+        #scanedVeldpar5k()
+        #ActivateMiners()
+    
 
 
 if __name__ == "__main__":
