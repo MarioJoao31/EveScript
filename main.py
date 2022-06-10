ae0b48c2997a3a0655d6953d6b285a8d9f94eaa2
MarioJoao31 <mario.joao31@hotmail.com>
2022-06-10 01:51:15 +0100

    main bot added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pyautogui,sys
 import time
 import discord
+from dotenv import load_dotenv
 import random
 import os
 
@@ -207,7 +208,23 @@ def ActivateMiners():
 
 
 def main():
-    
+    class MyClient(discord.Client):
+        async def on_ready(self):
+            print('Logged on as {0}!'.format(self.user))
+
+        async def on_message(self, message):
+            print('Message from {0.author}: {0.content}'.format(message))
+            if message.content == '!funcoes':
+                await message.channel.send(f'{message.author.name} as funções deste bot são as seguintes:{os.linesep}1-undock{os.linesep}2-minOverview{os.linesep}3-minBelt{os.linesep}4-warp{os.linesep}5-lockVeldspar{os.linesep}6-aproch{os.linesep}7-scan{os.linesep}8-scanedVeldpar5k{os.linesep}9-lock{os.linesep}10-ActivateMiners{os.linesep}11-fullOreHold')
+
+    load_dotenv()
+    client = MyClient()
+    #correr o bot no discord e vai buscar a pass ao .env
+    client.run(os.getenv("TOKEN"))
+
+
+
+
     #começar
     pyautogui.FAILSAFE = True
     #########foca na janela do EVE
